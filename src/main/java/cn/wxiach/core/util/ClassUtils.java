5688078baf215e89d9e74d77201733a609583889
wxiach <wxiach@gmail.com>
2025-02-11 14:47:33 +0800

    refactor: ClassUtils and ResourceUtils

--- src/main/java/cn/wxiach/core/util/ClassUtils.java
+++ src/main/java/cn/wxiach/core/util/ClassUtils.java
@@ -1,15 +1,22 @@
 package cn.wxiach.core.util;
 
+import java.util.Objects;
+
 /**
  * @author wxiach 2025/1/10
  */
 public class ClassUtils {
 
+    /**
+     * This method guarantees that a non-null ClassLoader is returned.
+     *
+     * @return a non-null ClassLoader
+     */
     public static ClassLoader getDefaultClassLoader() {
         ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        if (classLoader == null) {
+        if (Objects.isNull(classLoader)) {
             classLoader = ClassUtils.class.getClassLoader();
-            if (classLoader == null) {
+            if (Objects.isNull(classLoader)) {
                 classLoader = ClassLoader.getSystemClassLoader();
             }
         }
--- src/main/java/cn/wxiach/core/util/ResourceUtils.java
+++ src/main/java/cn/wxiach/core/util/ResourceUtils.java
@@ -21,11 +21,13 @@ public class ResourceUtils {
     private static final Path basePath;
 
     static {
+        // Get the default resource path from the class loader
         URL resource = ClassUtils.getDefaultClassLoader().getResource("");
+
         try {
             basePath = Paths.get(Objects.requireNonNull(resource).toURI()).normalize();
         } catch (URISyntaxException e) {
-            throw new RuntimeException(e);
+            throw new IllegalStateException("Invalid URI format for the resource path", e);
         }
     }
 
@@ -36,6 +38,18 @@ public class ResourceUtils {
     public static boolean checkClassResource(Path path) {
         return path.getFileName().toString().endsWith(CLASS_SUFFIX);
     }
+
+    /**
+     * Converts a file path of a compiled class to the corresponding Class resource.
+     *
+     * This method extracts the class name from the given file path, ensuring it matches
+     * the package structure and class name format required for class loading.
+     * The file path is expected to be relative to the base path.
+     *
+     * @param path The file path to the compiled class file (e.g., "com/example/MyClass.class").
+     * @return The corresponding Class object representing the class defined in the file.
+     * @throws ClassNotFoundException If the class cannot be loaded by the class loader.
+     */
 
     public static Class<?> convertToClassResource(Path path) throws ClassNotFoundException {
         String className = path.toAbsolutePath().toString()
